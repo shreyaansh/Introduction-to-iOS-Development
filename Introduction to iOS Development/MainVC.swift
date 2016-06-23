1a66559fcdbcf21b8b34fc8b143a110bda45901b
Shreyaansh Bassi <sbassi@purdue.edu>
2016-06-23 13:16:10 +0530

    Configuring it for iPad air

--- Introduction to iOS Development/MainVC.swift
+++ Introduction to iOS Development/MainVC.swift
@@ -9,37 +9,22 @@
 import UIKit
 
 class MainVC: UIViewController {
-
-    var messages : [String] = ["You really want an answer, don't you?", "My god you are desperate", "I mean how desperate can you be", "Still thinking, huh!"]
+    
+    var messages = ["You really want an answer, don't you?", "My god you are desperate", "I mean how desperate can you be", "Still thinking, huh!"]
     var counter : Int = 0
     
     @IBOutlet weak var MainLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     @IBAction func getAnswer(sender: UIButton) {
-        counter = counter == 4 ? 0 : counter
+        MainLabel.text = "Hey! It pressed."
+        /*counter = counter == 4 ? 0 : counter
         MainLabel.text = messages[counter]
-        counter = counter + 1
+        counter = counter + 1*/
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
 }
